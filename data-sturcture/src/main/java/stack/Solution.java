860be7014bec5f4657e6a4e29d52717c3ea11494
可超 陈 <648929783@qq.com>
2019-07-10 22:04:16 +0800

    括号匹配

---
+++ data-sturcture/src/main/java/stack/Solution.java
@@ -0,0 +1,41 @@
+package stack;
+
+import java.util.Stack;
+
+/**
+ * @author chenkechao
+ * @date 2019-07-10 21:57
+ */
+public class Solution {
+
+    /**
+     * 括号匹配
+     * @param s
+     * @return
+     */
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{' ) {
+                stack.push(c);
+            } else if (c == ')' || c == ']' || c == '}') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                Character pop = stack.pop();
+
+                if (c == ')' && pop != '(') {
+                    return false;
+                }
+                if (c == ']' && pop != '[') {
+                    return false;
+                }
+                if (c == '}' && pop != '{') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
