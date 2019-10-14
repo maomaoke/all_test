8b278dc376d385109f9063b8171b6c5c5d06c9fb
可超 陈 <648929783@qq.com>
2019-10-14 21:43:55 +0800

    改进归并排序 添加插入排序优化

--- algorithm/src/main/java/MergeSort.java
+++ algorithm/src/main/java/MergeSort.java
@@ -5,6 +5,9 @@ import java.util.Arrays;
  * @date 2019/10/11 8:28 下午
  */
 public class MergeSort {
+
+    private static final int DEFAULT_VALUE = 15;
+
     private MergeSort() {
     }
 
@@ -14,15 +17,21 @@ public class MergeSort {
 
     private static void mergeSort(Comparable[] arr, int left, int right) {
         //终止条件
-        if (left >= right) {
-            //只有一个数据
+//        if (left >= right) {
+//            //只有一个数据
+//            return;
+//        }
+        if (right - left <= DEFAULT_VALUE) {
+            insertionSort(arr, left, right);
             return;
         }
 
         int middle = left + (right - left) / 2;
         mergeSort(arr, left, middle);
         mergeSort(arr, middle + 1, right);
-        merge(arr, left, middle, right);
+        if (arr[middle].compareTo(arr[middle + 1]) > 0) {
+            merge(arr, left, middle, right);
+        }
     }
 
     private static void merge(Comparable[] arr, int l, int mid, int r) {
@@ -47,8 +56,28 @@ public class MergeSort {
         }
     }
 
+    /**
+     * 删除排序
+     * @param arr
+     * @param left
+     * @param right
+     */
+    private static void insertionSort(Comparable[] arr, int left, int right) {
+
+        for (int i = left + 1; i <= right ; i++) {
+            Comparable tmp = arr[i];
+            int j;
+            for (j = i; j > left && arr[j -1].compareTo(arr[j]) > 0; j --) {
+                arr[j] = arr[j - 1];
+            }
+            arr[j] = tmp;
+        }
+    }
+
+
+
     public static void main(String[] args) {
-        Integer[] testArray = SortTestHelper.generateRandomArray(8, 0, 10000);
+        Integer[] testArray = SortTestHelper.generateRandomArray(1000, 0, 10000);
 
         MergeSort.sort(testArray);
         SortTestHelper.printArray(testArray);
