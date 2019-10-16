f00961e799d6e9f8d75e51631add7772232f7363
可超 陈 <648929783@qq.com>
2019-10-16 22:33:36 +0800

    双路快速排序

--- algorithm/src/main/java/QuickSort.java
+++ algorithm/src/main/java/QuickSort.java
@@ -1,3 +1,5 @@
+import java.io.PipedReader;
+
 /**
  * @author chenkechao
  * @date 2019/10/14 9:44 下午
@@ -13,7 +15,8 @@ public class QuickSort {
 
     private static <E> void sort(Comparable<E>[] arr, int left, int right) {
 
-        if (left >= right) {
+        if (right - left <= 15) {
+            insertionSort(arr, left, right);
             return;
         }
 
@@ -23,16 +26,52 @@ public class QuickSort {
         sort(arr, p + 1, right);
     }
 
+//    private static <E> int partition(Comparable<E>[] arr, int left, int right) {
+//
+//        // 随机在arr[l...r]的范围中, 选择一个数值作为标定点pivot
+//        swap(arr, left, (int) (Math.random() * (right - left + 1)) + left);
+//
+//        Comparable<E> v = arr[left];
+//
+//        int j = left;
+//        for (int i = left + 1; i <= right; i++) {
+//            if (arr[i].compareTo((E) v) < 0) {
+//                swap(arr, i, j + 1);
+//                j++;
+//            }
+//        }
+//        swap(arr, left, j);
+//        return j;
+//    }
+
+    /**
+     * 双路排序
+     *
+     * @param arr
+     * @param left
+     * @param right
+     * @param <E>
+     * @return
+     */
     private static <E> int partition(Comparable<E>[] arr, int left, int right) {
 
+        // 随机在arr[l...r]的范围中, 选择一个数值作为标定点pivot
+        swap(arr, left, (int) (Math.random() * (right - left + 1)) + left);
         Comparable<E> v = arr[left];
 
-        int j = left;
-        for (int i = left + 1; i <= right; i++) {
-            if (arr[i].compareTo((E) v) < 0) {
-                swap(arr, i, j + 1);
-                j++;
+        //arr[left + 1 ... i) <= v and arr(j ... r] >= v
+        int i = left + 1, j = right;
+        for (; ; ) {
+            while (i <= right && arr[i].compareTo((E) v) < 0) {
+                i++;
             }
+            while (j >= left + 1 && arr[j].compareTo((E) v) > 0) {
+                j--;
+            }
+            if (i > j) {
+                break;
+            }
+            swap(arr, i++, j--);
         }
         swap(arr, left, j);
         return j;
@@ -42,6 +81,17 @@ public class QuickSort {
         Comparable tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
+    }
+
+    private static void insertionSort(Comparable[] arr, int left, int right) {
+        for (int i = left + 1; i <= right; i++) {
+            Comparable tmp = arr[i];
+            int j;
+            for (j = i; j > left && arr[j - 1].compareTo(tmp) > 0; j--) {
+                arr[j] = arr[j - 1];
+            }
+            arr[j] = tmp;
+        }
     }
 
     public static void main(String[] args) {
